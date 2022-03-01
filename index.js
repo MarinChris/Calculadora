00243c5a4b6d0657c5e9124f93a13a9e60f18935
Christian Marin <christian.com.n@gmail.com>
2022-02-28 19:49:38 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,139 @@
+var Mousetrap = require('mousetrap');
+
+var botonUno = document.getElementById('botonUno')
+var botonDos = document.getElementById('botonDos')
+var botonTres = document.getElementById('botonTres')
+var botonCuatro = document.getElementById('botonCuatro')
+var botonCinco = document.getElementById('botonCinco')
+var botonSeis = document.getElementById('botonSeis')
+var botonSiete = document.getElementById('botonSiete')
+var botonOcho = document.getElementById('botonOcho')
+var botonNueve = document.getElementById('botonNueve')
+var botonCero = document.getElementById('botonCero')
+
+var botonSuma = document.getElementById('botonSuma')
+var botonResta = document.getElementById('botonResta')
+var botonDiv = document.getElementById('botonDiv')
+var botonMult = document.getElementById('botonMult')
+
+var actual = document.getElementById('actual')
+var resultado = document.getElementById('resultado')
+
+var borrar = document.getElementById('actual')
+var d_elete = document.getElementById('resultado')
+
+botonUno.addEventListener('click', clickUno)
+botonDos.addEventListener('click', clickDos)
+botonTres.addEventListener('click', clickTres)
+botonCuatro.addEventListener('click', clickCuatro)
+botonCinco.addEventListener('click', clickCinco)
+botonSeis.addEventListener('click', clickSeis)
+botonSiete.addEventListener('click', clickSiete)
+botonOcho.addEventListener('click', clickOcho)
+botonNueve.addEventListener('click', clickNueve)
+botonCero.addEventListener('click', clickCero)
+
+botonSuma.addEventListener('click', clickSuma)
+botonResta.addEventListener('click', clickResta)
+botonDiv.addEventListener('click', clickDiv)
+botonMult.addEventListener('click', clickMult)
+botonBorrar.addEventListener('click', clickBorrar)
+botonDelete.addEventListener('click', clickDelete)
+
+Mousetrap.bind('1', clickUno)
+Mousetrap.bind('2', clickDos)
+Mousetrap.bind('3', clickTres)
+Mousetrap.bind('4', clickCuatro)
+Mousetrap.bind('5', clickCinco)
+Mousetrap.bind('6', clickSeis)
+Mousetrap.bind('7', clickSiete)
+Mousetrap.bind('8', clickOcho)
+Mousetrap.bind('9', clickNueve)
+Mousetrap.bind('0', clickCero)
+
+Mousetrap.bind('+', clickSuma)
+Mousetrap.bind('-', clickResta)
+Mousetrap.bind('*', clickMult)
+Mousetrap.bind('/', clickDiv)
+
+function clickUno (){
+    actual.innerHTML+='1'
+}
+
+function clickDos (){
+    actual.innerHTML+='2'
+}
+
+function clickTres (){
+    actual.innerHTML+='3'
+}
+
+function clickCuatro (){
+    actual.innerHTML+='4'
+}
+
+function clickCinco (){
+    actual.innerHTML+='5'
+}
+
+function clickSeis (){
+    actual.innerHTML+='6'
+}
+
+function clickSiete (){
+    actual.innerHTML+='7'
+}
+
+function clickOcho (){
+    actual.innerHTML+='8'
+}
+
+function clickNueve (){
+    actual.innerHTML+='9'
+}
+
+function clickCero (){
+    actual.innerHTML+='0'
+}
+
+function clickSuma (){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorActual + valorResultado
+    actual.innerHTML = '0'
+}
+
+function clickResta (){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorActual - valorResultado
+    actual.innerHTML = '0'
+}
+
+function clickDiv (){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorActual / valorResultado
+    actual.innerHTML = '0'
+}
+
+function clickMult (){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = valorActual * valorResultado
+    actual.innerHTML = '0'
+}
+
+function clickBorrar(){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    actual.innerHTML = '0'
+}
+
+function clickDelete(){
+    let valorActual = parseInt(actual.innerHTML)
+    let valorResultado = parseInt(resultado.innerHTML)
+    resultado.innerHTML = '0'
+    actual.innerHTML = '0'
+}
+
---
+++ main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require ('electron');
+
+/*function createWindow(){
+    const ventana = new BrowserWindow({
+        width: 300,
+        height: 250
+    });
+    ventana.loadFile('index.html')
+}*/
+
+function createWindow(){
+    const ventana = new BrowserWindow({
+    width : 350,
+    height : 250,
+        webPreferences: {
+            nodeIntegration: true, 
+            contextIsolation: false
+        }
+    
+    })
+    ventana.loadFile('index.html')
+    }
+
+
+app.whenReady().then(createWindow)
